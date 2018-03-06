51ca5b44af142d382fead4b3ae0f23a6ed684cc4
Jim Bevington <jim.bevington@hotmail.com>
2018-03-06 18:51:13 +0000

    setup empty PersonTest and GermTest with Setup method

---
+++ src/test/java/GermTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class GermTest {
+
+    private Germ germ;
+
+    @Before
+    public void setUp() throws Exception {
+        germ = new Germ("Gary", 12);
+    }
+
+
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,20 @@
+public class PersonTest {
+
+//    Person
+//        has name, health and body with Combatants in it
+
+//    when catches a Disease
+//    adds Germ to body
+//    Germs deadliness docks Health
+
+//    germs can spawn, docking Health more
+//              mutate, increasing Deadliness
+//              attack nanobots
+
+//    when drinks some Nanobots
+//    adds nanobot to the body
+
+
+//    nanobot can attack disease
+//                disperse medicine
+}
